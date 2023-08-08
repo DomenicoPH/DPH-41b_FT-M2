68e0f89a4b6aa951f11b9b49ba9e169703b2863c
Domenico <dpaganoh@gmail.com>
2023-08-08 08:43:41 -0500

    Homework 06 - React Intro

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Domenico";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1>Títulos</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills && 
+          techSkills.map((element,index) => {
+            return <li key={index}>{element}</li>;
+          })}
+      </ul>
+      <Botones alerts={alerts}></Botones>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Botones extends React.Component {
+  render() {
+    //const {alerts} = this.props
+    const {m1,m2} = this.props.alerts
+    return (
+      <div>
+        <button onClick={() => alert(m1)}>Módulo 1</button>
+        <button onClick={() => alert(m2)}>Módulo 2</button>
+      </div>
+    )
+  }
+}
